a73e83c6339d346bd0023a1aa122c94e267c4dd4
schoolexploitkid <goofyguy_69420@outlook.com>
2024-11-07 10:16:56 -0800

    Update ltbeef.js

--- ltbeef.js
+++ ltbeef.js
@@ -1,1 +1,17 @@
-
+var a = prompt('Extension ID of extension to toggle. WARNING: You must enable flag extensions-on-chrome-urls, then execute this code on chrome://extensions');
+var b = prompt('On or Off? ON (all caps) for on, and OFF (all caps) for off.');
+var c = false;
+if (b == "ON") {
+  c = true;
+} else if (b == "OFF") {
+  c = false;
+} else {
+  c = false;
+  alert('On or off input is not valid. Defaulting to off.');
+}
+try {
+  chrome.management.setEnabled(a, c);
+  console.log('Extension ' + a + ' was turned ' + c + ' successfully.')
+} catch (e) {
+  alert('An error occured:' + e);
+}
