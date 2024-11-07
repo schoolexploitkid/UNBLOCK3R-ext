17918c01badf9f2ee13f1e522b0d062bb64f2248
schoolexploitkid <goofyguy_69420@outlook.com>
2024-11-06 17:17:28 -0800

    Create unblock.js
    blocks requests from filtering sites

---
+++ unblock.js
@@ -0,0 +1,10 @@
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+    // Block requests to any ads-related domain
+    if (details.url.includes('securly.com') || details.url.includes('goguardian.com') || details.url.includes('lanschool.com') || details.url.includes('linewize.com') || details.url.includes('blocksi.net') || details.url.includes('fortinet.com') || details.url.includes('cisco.com') || details.url.includes('imperosoftware.com') || details.url.includes('hapara.com') || details.url.includes('iboss.com') || details.url.includes('lightspeedsystems.com')) {
+      return { cancel: true };
+    }
+  },
+  { urls: ["<all_urls>"] },  // Match all URLs
+  ["blocking"]
+);
