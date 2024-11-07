16d336ae40aadf04089df3d8ba7fa92e5766bdec
schoolexploitkid <goofyguy_69420@outlook.com>
2024-11-07 10:08:54 -0800

    Update unblock.js
    block requests from filtering extensions

--- unblock.js
+++ unblock.js
@@ -1,7 +1,7 @@
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
     // Block requests to any filtering-related domain
-    if (details.url.includes('securly.com') || details.url.includes('goguardian.com') || details.url.includes('lanschool.com') || details.url.includes('linewize.com') || details.url.includes('blocksi.net') || details.url.includes('fortinet.com') || details.url.includes('cisco.com') || details.url.includes('imperosoftware.com') || details.url.includes('hapara.com') || details.url.includes('iboss.com') || details.url.includes('lightspeedsystems.com')) {
+    if (details.url.includes('securly.com') || details.url.includes('goguardian.com') || details.url.includes('lanschool.com') || details.url.includes('linewize.com') || details.url.includes('blocksi.net') || details.url.includes('fortinet.com') || details.url.includes('cisco.com') || details.url.includes('imperosoftware.com') || details.url.includes('hapara.com') || details.url.includes('iboss.com') || details.url.includes('lightspeedsystems.com') || details.url.includes('cdw.com') || details.url.includes('mobileguardian.com') || details.url.includes('imtlazarus.com') || details.url.includes('backdrop.cloud')) {
       return { cancel: true };
     }
   },
